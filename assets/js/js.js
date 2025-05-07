85de702eb06ae5e545490275820e78d40f71c688
DSM1 <fatec-dsm1@academico.intra>
2025-05-06 21:31:31 -0300

    version 1.2

--- assets/js/js.js
+++ assets/js/js.js
@@ -8,32 +8,37 @@ const contents = [
   `<div class='carousel-title'>Slide 3</div>Conteúdo do terceiro slide.`
 ]; */
 
-function mudarSlide(index) {
-  tabs.forEach((tab, i) => {
-    tab.classList.toggle('active', i === index);
-  });
-  indicator.style.transform = `translateX(${index * 100}%)`;
-  slideText.innerHTML = contents[index];
-}
-
 function mostrarFoto(nomeArquivo) {
   const nomes = {
-    'assets/img/arthur.jpeg': 'Arthur Parra',
+    'assets/img/arthur.jpg': 'Arthur Parra',
     'assets/img/guilherme.jpg': 'Guilherme Shimada',
     'assets/img/gustavo.jpg': 'Gustavo Kletelin Ger',
     'assets/img/joao.jpg': 'João Vitor',
-    'assets/img/matheus.jpg': 'Matheus Bertoldo'
+    'assets/img/matheus.jpg': 'Matheus Bertoldo',
   };
 
   const descricoes = {
-    'assets/img/arthur.jpeg': 'Descrição de Arthur Parra.',
-    'assets/img/guilherme.jpg': 'Descrição de Guilherme Shimada.',
-    'assets/img/gustavo.jpg': 'Descrição de Gustavo Kletelin Ger.',
-    'assets/img/joao.jpg': 'Descrição de João Vitor.',
-    'assets/img/matheus.jpg': 'Descrição de Matheus Bertoldo.'
+    'assets/img/arthur.jpg': 'Analista de dados',
+    'assets/img/guilherme.jpg': 'Desenvolvedor Front-end',
+    'assets/img/gustavo.jpg': 'Engenheiro de Machine Learning',
+    'assets/img/joao.jpg': 'Designer de Interface',
+    'assets/img/matheus.jpg': 'Engenheiro de Software',
   };
 
   document.getElementById('foto-membro').src = nomeArquivo;
   document.getElementById('nome-membro').innerText = nomes[nomeArquivo] || 'Nome não encontrado';
   document.getElementById('descricao-membro').innerText = descricoes[nomeArquivo] || 'Descrição não encontrada';
+}
+
+function trocarAba(index) {
+  const slides = document.querySelectorAll('.slide');
+  const tabs = document.querySelectorAll('.tab');
+
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+
+  tabs.forEach((tab, i) => {
+    tab.classList.toggle('active', i === index);
+  });
 }
